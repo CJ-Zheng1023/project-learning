a2b2f37844549df021a6418487c48521d57daa47
AfterWin <CJ_Zheng1023@hotmail.com>
2016-11-29 11:48:26 +0800

    添加req.session的例子

--- express-example-request/app.js
+++ express-example-request/app.js
@@ -2,10 +2,20 @@
 var express = require('express');
 var app = express();
 
+//引入session中间件
+var session = require('express-session');
 //引入body-parser模块，并设置中间件，该中间件必须在所有中间件和路由之前引入
 //作用：处理post请求参数
 app.use(require('body-parser')());
 
+//session配置
+app.use(session({
+    secret: '123',
+    cookie: {maxAge: 3600000},
+    rolling: true,
+    resave: true,
+    saveUninitialized: true
+}));
 //配置静态资源
 app.use(express.static(__dirname+'/static'));
 
@@ -23,6 +33,11 @@ app.get('/query', function(req, res){
 app.post('/body', function(req, res){
     console.log(req.body.userName);
     res.send("body:"+req.body.userName);
+})
+//req.session 例子
+app.get('/session', function(req, res){
+    req.session.name = "zzzzz"
+    res.send("session:"+req.session.name);
 })
 
 //配置服务器监听，端口为3000
